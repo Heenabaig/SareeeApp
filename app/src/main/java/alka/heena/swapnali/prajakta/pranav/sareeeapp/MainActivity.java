9bc58585bc0bebd41b64fd7985c99797431a1d90
Heenabaig <heenabaig317@gmail.com>
2020-05-01 20:02:57 +0530

    Initial commit

---
+++ app/src/main/java/alka/heena/swapnali/prajakta/pranav/sareeeapp/MainActivity.java
@@ -0,0 +1,14 @@
+package alka.heena.swapnali.prajakta.pranav.sareeeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
